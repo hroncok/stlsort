573f44307c75c0d8740deeb0f850ee9a7c827cf9
Miro Hrončok <miro@hroncok.cz>
2014-09-01 19:39:31 +0200

    Updated setup.py a bit

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-try:
-    from setuptools import setup, find_packages
-except:
-    from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='stlsort',
     version='0.0.1',
-    description='Short description',
+    description='Sort ASCII STL files for better version control',
     long_description=''.join(open('README.rst').readlines()),
-    keywords='some, keywords',
-    author='yourname',
-    author_email='yourmail',
+    keywords='STL, mesh, 3D, sort, git',
+    author='nop head',
+    author_email='nop.head@gmail.com',
+    maintainer='Miro Hrončok',
+    maintainer_email='Miro Hrončok',
     license='GPLv2',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         ]
 )
